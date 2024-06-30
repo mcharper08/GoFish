4ed672e63aef41e0688d2d9275149f0829d01da6
mcharper08 <harpermegan15@gmail.com>
2024-06-30 13:21:06 -0400

    go fish added as text

---
+++ variables.py
@@ -0,0 +1,8 @@
+print(""" 
+        GGGGG OOOOO   FFFFF I SSSSS H   H !
+        G     O   O   F     I S     H   H !
+        G  GG O   O   FFF   I SSSSS HHHHH !
+        G   G O   O   F     I     S H   H
+        GGGGG OOOOO   F     I SSSSS H   H !""")
+
+
