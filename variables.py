c6156d287f0fca4d089860d3579d1721914816e1
mcharper08 <harpermegan15@gmail.com>
2024-08-25 21:11:38 -0400

    established while loops and debugged

--- variables.py
+++ variables.py
@@ -19,7 +19,7 @@ player_2_matches = 0
 
 #card variables
 suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
-kinds = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
+kinds = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 
 #create nested list named deck that has an identity 1-52 for each card suit and kind
 deck = [] #open list for editing
@@ -40,16 +40,16 @@ player_2_hand = deck[7:14] # player 2 gets dealt cards 8-14
 stock_pile = deck[14:] # stock pile is the remaining cards left
 
 # clears the screen of displaying cards
-def clear_console():
-    os.system('cls' if os.name == 'nt' else 'clear')
+#def clear_console():
+    #os.system('cls' if os.name == 'nt' else 'clear')
 
 #displays player 1's cards
 def display_hands(player_name, hand):
-    clear_console()
+    #clear_console()
     input(f"{player_name}, click Enter to see your hand")
     print(f"{player_name}'s hand is: {hand}")
     input(f"{player_name} press Enter when done viewing")
-    clear_console()
+    #clear_console()
 
 #checks for matches and updates hand to display what has no match yet
 def matches_check(hand):
@@ -71,19 +71,46 @@ def go_fish(player_name, hand):
 def turn(player_name, player_hand, opponent_hand):
     display_hands(player_name, player_hand) #show the current player their hand
     do_you_have = input(f"{player_name}, what kind are you asking from your opponent?") #ask the player what card they would like from their opponent
+    while do_you_have not in kinds:
+        print('Invalid input, try again!')
+        do_you_have = input(f"{player_name}, what kind are you asking from your opponent?") 
     opponent_kinds = [card[2] for card in opponent_hand] #searches opponent's hand for kinds
+    match_boolean = False
+
     for card in opponent_hand:
         if card[2] == do_you_have: #if requested kind is a match in the opponent's hand...
             player_hand.append(card) #the player gets the card
             opponent_hand.remove(card) #the opponent loses the card
-        else:
-            print("Go Fish!") #if the requested kind is not in the opponent's hand
-            go_fish(player_name, player_hand) #the player must pick from the stock pile 
+            match_boolean = True
+            print(f"{do_you_have} was found!")
+    if match_boolean == False:
+        print("Go Fish!") #if the requested kind is not in the opponent's hand
+        go_fish(player_name, player_hand) #the player must pick from the stock pile 
 
 #create matches to keep track of the game
 player_1_matches = []
 player_2_matches = []
 
 #establish the game of go fish using a while loop
+while len(stock_pile) > 0 and (len(player_1_hand) > 0 and len(player_2_hand) > 0): #establish the game
+    turn(player_1_name, player_1_hand, player_2_hand)
+    player_1_matches += matches_check(player_1_hand)
 
+    if len(player_1_hand) == 0:
+        print(f'{player_1_name} + is out of cards!')
+        break #if this occurs, the game is over
+    
+    turn(player_2_name, player_2_hand, player_1_hand)
+    player_2_matches += matches_check(player_2_hand)
+              
+    if len(player_2_hand) == 0:
+        print(f"{player_2_name} is out of cards!")
+        break #if this occurs, the game is over
 
+#Determine winner of the game
+if len(player_1_matches) > len (player_2_matches):
+    print(f"{player_1_name} is the winner!")
+elif len(player_2_matches) < len(player_1_matches):
+    print(f"{player_2_name} is the winner!")
+else:
+    print("It's a tie!")
